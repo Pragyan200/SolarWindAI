83d6b70055dc90e37a0e0e78a937036892e4dbdf
Pragyan200 <pragyanpmohanty4@gmail.com>
2026-07-08 19:03:44 +0530

    added project and sites APIS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.api.home import router as home_router
+from app.api.projects import router as projects_router
+from app.api.sites import router as sites_router
+
 app=FastAPI()
-@app.get("/")
-def home():
-    return{
-        "message":"Solar & Wind Deployment Intelligence Platform"
-    }
+
+app.include_router(home_router)
+app.include_router(projects_router)
+app.include_router(sites_router)
+
 @app.get("/health")
 def health():
     return{
@@ -13,5 +17,5 @@ def health():
 @app.get("/about")
 def about():
     return{
-        "project":"Solar & Wind Deployment Intelligence Platform"
+        "project":"Solar & Wind Deployment Intelligence Platforms"
     }
